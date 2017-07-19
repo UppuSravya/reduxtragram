242835748b0a772aac25259ffe67cafadb5a69ab
Kiv Lo <kivlokw@gmail.com>
2017-07-19 16:44:09 -0400

    connect store , actions and reducers to app

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import { Link } from 'react-router';
+
+class PhotoGrid extends Component {
+  render() {
+    return (
+      <div className="photo-grid">
+        I am  the photo grid. 
+      </div>
+    );
+  }
+}
+
+
+export default PhotoGrid;
---
+++ client/components/Single.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import { Link } from 'react-router';
+
+class Single extends Component {
+  render() {
+    return (
+      <div className="single-photo">
+        I am a single
+      </div>
+    );
+  }
+}
+
+
+export default Single;
