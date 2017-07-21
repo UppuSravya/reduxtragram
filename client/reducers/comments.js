093485104f2ef30c1a51af9a4ed6dc6c6e4a6c65
Kiv Lo <kivlokw@gmail.com>
2017-07-21 14:31:53 -0400

    fix syntax

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,6 +1,30 @@
+function postComments(state = [] , action){
+  switch(action.type){
+    case 'ADD_COMMENT':
+    //return the new state with the new comments
+      return [...state, {
+        user: action.author,
+        text: action.comment
+      }];
 
-
-function comments(state = [], action) {
+    case 'REMOVE_COMMENT':
+      return state;
+    default:
+      return state;
+  }
   return state;
 }
+
+function comments(state = [], action) {
+  if(typeof action.postId !== 'undefined'){
+    return {
+      //take the current state
+      ...state,
+      // overwrite this post with a new one
+      [action.postId]: postComments(state[action.postId], action)
+    }
+  }
+  return state;
+}
+
 export default comments;
